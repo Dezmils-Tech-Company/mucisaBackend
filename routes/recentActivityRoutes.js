e43cffae675b3578d7250a0a1002ba6241c0d197
Dezmils-Tech-Company <dez.ezra05@gmail.com>
2025-07-22 12:40:30 +0300

    express

--- routes/recentActivityRoutes.js
+++ routes/recentActivityRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getActivities } = require('../controllers/recentactivityController');
+const { getActivities } = require('../controllers/recentActivityController');
 
 router.get('/', getActivities);
 module.exports = router;
